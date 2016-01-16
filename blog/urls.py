9fce7d740254b5fce41220aae623549d48f0eb50
Sofi <sofidenner@gmail.com>
2016-01-16 17:03:48 -0300

    adding comment model

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name ='post_remove'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^post/(?P<pk>[0-9]+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>[0-9])+/approve/$', views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<pk>[0-9])+/remove/S', views.comment_remove, name='comment_remove'),
 ]
